ffdf8593e4fc6e534058c1f0e45d1ae3b1d84ee0
Kyle's Desktop <khansen804@yahoo.com>
2022-02-19 00:49:47 -0700

    Banner completed

--- Chamber/js/banner.js
+++ Chamber/js/banner.js
@@ -4,14 +4,13 @@ const banner = document.getElementById("myHeader");
 if (weekday == 2 || weekday == 3){
     //https://www.w3schools.com/js/js_htmldom_nodes.asp
     const para = document.createElement("p");
-    const node = document.createTextNode("Come join us at the meeting... wherever it is.");
+    const node = document.createTextNode("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.");
     para.appendChild(node);
     const element = document.getElementById("myHeader");
     element.appendChild(para);
     banner.className = "on";
 }else {
     banner.className = "off";
+
 }
-
-
 
